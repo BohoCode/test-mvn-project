8a9fc5e1790f17e3aec7c7935b68fbcb9e5e7639
JamieB <jamie.bowen@forgerock.com>
2020-03-06 15:52:48 +0000

    Project now builds

---
+++ src/main/java/TestActions.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2018 ForgeRock AS. All Rights Reserved
+ * <p>
+ * Use of this code requires a commercial software license with ForgeRock AS.
+ * or with one of its affiliates. All use shall be exclusively subject
+ * to such license between the licensee and ForgeRock AS.
+ */
+
+public class TestActions {
+}
