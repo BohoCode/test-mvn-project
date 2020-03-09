ec92e99d1269679f2df842005cfb8427636b9b90
JamieB <jamie.bowen@forgerock.com>
2020-03-09 14:32:13 +0000

    Try stuff

--- src/test.sh
+++ src/test.sh
@@ -3,6 +3,6 @@ set -e
 
 
 GITHUB_WORKSPACE=/home/jamie
-LOCAL_REPO_PATH='$GITHUB_WORKSPACE/.m2/repository'
-LOCAL_REPO_PATH_ARG= echo `eval $LOCAL_REPO_PATH`
+LOCAL_REPO_PATH='$GITHUB_WORKSPACE/.m2/repositoryy'
+LOCAL_REPO_PATH_ARG= `eval echo $LOCAL_REPO_PATH`
 mvn -X $LOCAL_REPO_PATH_ARG clean install
